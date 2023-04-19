ac36576e4938e63f58bf52956e995dfa3e289ade
Pedro Batista <pedro.batista@ericsson.com>
2023-04-19 13:49:23 +0200

    Adds script to run multiple experiments

---
+++ run_graph_filter_experiments.py
@@ -0,0 +1,18 @@
+from mainOnlyFilter import mainOnlyFilter
+from mainOnlyPerceptron import mainOnlyPerceptron
+import lab2_graph_filter_train
+
+experiments = {
+    'Reference filter': mainOnlyFilter.run,
+    'Reference perceptron': mainOnlyPerceptron.run,
+    'My filter': lambda: lab2_graph_filter_train.run('graph_filter'),
+    'My perceptron': lambda: lab2_graph_filter_train.run('graph_perceptron'),
+}
+
+n_experiments = 30
+for name, experiment in experiments.items():
+    result_sum = 0
+    for _ in range(n_experiments):
+        result_sum += experiment()
+    result_avg = result_sum / n_experiments
+    print(f'{result_avg} - {name}')
