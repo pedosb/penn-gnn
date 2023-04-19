742d53123660feffaf07863ba75efcad45794306
Pedro Batista <pedro.batista@ericsson.com>
2023-04-19 13:47:50 +0200

    Makes train load saved dataset and make the training a function

--- lab2_graph_filter_train.py
+++ lab2_graph_filter_train.py
@@ -1,4 +1,3 @@
-# %%
 import torch
 from matplotlib import pyplot as plt
 import numpy as np
@@ -14,59 +13,64 @@ n_epochs = 30
 batch_size = 200
 validation_ration = 0.1
 
-graph_shift_operator, X_train, X_test, Y_train, Y_test = [
-    torch.tensor(v) for v in generate_dataset()
-]
+graph_shift_operator, X_train, X_test, Y_train, Y_test = torch.load('lab2_dataset_reference.pt')
+
 train_size = int(np.floor((1 - validation_ration) * X_train.shape[0]))
 X_train, X_validation = torch.split(X_train, train_size)
 Y_train, Y_validation = torch.split(Y_train, train_size)
 
-# %%
-TASK = 'multilayer_gnn'
-if TASK == 'graph_filter':
-    filter_model = GraphFilter(filter_order, graph_shift_operator, use_activation=False)
-elif TASK == 'graph_perceptron':
-    filter_model = GraphFilter(filter_order, graph_shift_operator, use_activation=True)
-elif TASK == 'multilayer_gnn':
-    filters_order = [8, 1]
-    filter_model = MultiLayerGNN(filters_order, graph_shift_operator)
 
-optimizer = torch.optim.Adam(filter_model.parameters(), learning_rate)
-loss_function = torch.nn.MSELoss(reduction='mean')
+def run(task, verbose=False):
+    if task == 'graph_filter':
+        filter_model = GraphFilter(filter_order, graph_shift_operator, use_activation=False)
+    elif task == 'graph_perceptron':
+        filter_model = GraphFilter(filter_order, graph_shift_operator, use_activation=True)
+    elif task == 'multilayer_gnn':
+        filters_order = [8, 1]
+        filter_model = MultiLayerGNN(filters_order, graph_shift_operator)
 
-# %%
-batch_loss_history = []
-validation_loss_history = []
-step = 0
-for epoch in range(n_epochs):
-    for batch_idx in torch.split(torch.randperm(X_train.shape[0]), batch_size):
-        X_batch = X_train[batch_idx]
-        Y_batch = Y_train[batch_idx]
-        predicted = filter_model.forward(X_batch)
-        loss = loss_function(Y_batch, predicted)
-        batch_loss_history.append((step, loss.detach().numpy()))
-        filter_model.zero_grad()
-        loss.backward()
-        optimizer.step()
-        step += 1
+    optimizer = torch.optim.Adam(filter_model.parameters(), learning_rate)
+    loss_function = torch.nn.MSELoss(reduction='mean')
+
+    batch_loss_history = []
+    validation_loss_history = []
+    step = 0
+    for epoch in range(n_epochs):
+        for batch_idx in torch.split(torch.randperm(X_train.shape[0]), batch_size):
+            X_batch = X_train[batch_idx]
+            Y_batch = Y_train[batch_idx]
+            predicted = filter_model.forward(X_batch)
+            loss = loss_function(Y_batch, predicted)
+            batch_loss_history.append((step, loss.detach().numpy()))
+            filter_model.zero_grad()
+            loss.backward()
+            optimizer.step()
+            step += 1
+        with torch.no_grad():
+            predicted = filter_model.forward(X_validation)
+            validation_loss_history.append((step, loss_function(Y_validation, predicted).numpy()))
+
+    if verbose:
+        fig = plt.figure()
+        ax = fig.add_subplot(1, 1, 1)
+
+        # ax.set_yscale('log')
+        ax.plot(*np.array(batch_loss_history).T, label='Batch loss')
+        ax.plot(*np.array(validation_loss_history).T, label='Validation loss')
+        ax.legend()
+
     with torch.no_grad():
-        predicted = filter_model.forward(X_validation)
-        validation_loss_history.append((step, loss_function(Y_validation, predicted).numpy()))
+        predicted = filter_model.forward(X_test)
+        test_loss = loss_function(Y_test, predicted)
+        if verbose:
+            print(f'Test loss {test_loss}')
+            print(f'Validation loss {validation_loss_history[-1][1]}')
+            predicted = filter_model.forward(X_train)
+            train_loss = loss_function(Y_train, predicted)
+            print(f'Train loss {train_loss}')
+            plt.show()
+    return test_loss
 
-# %%
-fig = plt.figure()
-ax = fig.add_subplot(1, 1, 1)
 
-# ax.set_yscale('log')
-ax.plot(*np.array(batch_loss_history).T, label='Batch loss')
-ax.plot(*np.array(validation_loss_history).T, label='Validation loss')
-ax.legend()
-
-with torch.no_grad():
-    predicted = filter_model.forward(X_test)
-    test_loss = loss_function(Y_test, predicted)
-    print(f'Test loss {test_loss}')
-    print(f'Validation loss {validation_loss_history[-1][1]}')
-    predicted = filter_model.forward(X_train)
-    train_loss = loss_function(Y_train, predicted)
-    print(f'Train loss {train_loss}')
+if __name__ == '__main__':
+    run('graph_perceptron', True)
