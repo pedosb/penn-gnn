6d073ee3d66ce617df1555d51db89a757dce28a4
Pedro Batista <pedro.batista@ericsson.com>
2023-05-03 15:58:20 +0200

    Generates lab 3 train/test data up to 1.6

--- lab3_dataset.py
+++ lab3_dataset.py
@@ -1,6 +1,8 @@
 import numpy as np
 import zipfile
 
+from utils import random
+
 
 def load_movie_dataset():
     with zipfile.ZipFile('data/ml-100k.zip') as zipf:
@@ -24,3 +26,23 @@ def load_movie_dataset():
     cleaned_movie_ratings = movie_ratings[:, selected_movies_mask]
 
     return cleaned_movie_ratings, new_movie_id_map
+
+
+def split_train_test_users(n_users, ratio_train=0.9):
+    n_train = int(np.floor(ratio_train * n_users))
+    random_indexes = np.arange(n_users)
+    random.shuffle(random_indexes)
+    users_train, users_test = random_indexes[:n_train], random_indexes[n_train:]
+    return users_train, users_test
+
+
+def remove_users_not_rating_movie(movie_ratings, movie):
+    return movie_ratings[movie_ratings[:, movie] != 0]
+
+
+def create_samples_from_ratings(movie_ratings, target_movie):
+    X = movie_ratings.copy()
+    X[:, target_movie] = 0
+    Y = np.zeros_like(X)
+    Y[:, target_movie] = movie_ratings[:, target_movie]
+    return X, Y
--- lab3_main.py
+++ lab3_main.py
@@ -1,22 +1,33 @@
 # %%
 import numpy as np
-from lab3_dataset import load_movie_dataset
-from lab3_similarity_graph import compute_movie_similarity_graph, stratify_and_normalize
 
+from lab3_dataset import (create_samples_from_ratings, load_movie_dataset,
+                          remove_users_not_rating_movie, split_train_test_users)
+from lab3_similarity_graph import (compute_movie_similarity_graph, stratify_and_normalize)
 from utils import random
 
+# %%
 contact_index = 257
 
 movie_ratings, movie_id_map_loading = load_movie_dataset()
 contact_index = movie_id_map_loading[contact_index]
 n_users = movie_ratings.shape[0]
 
-n_train = int(np.floor(.9 * n_users))
-random_indexes = np.arange(n_users)
-random.shuffle(random_indexes)
-train_users, test_users = random_indexes[:n_train], random_indexes[n_train:]
+users_train, users_test = split_train_test_users(n_users)
 
-adjacency_matrix = compute_movie_similarity_graph(movie_ratings[train_users])
+adjacency_matrix = compute_movie_similarity_graph(movie_ratings[users_train])
 print(f'There are {adjacency_matrix.shape[0]} movies in the similarity graph')
 
 adjacency_matrix = stratify_and_normalize(adjacency_matrix)
+
+# %%
+movie_ratings_train = remove_users_not_rating_movie(movie_ratings[users_train], contact_index)
+movie_ratings_test = remove_users_not_rating_movie(movie_ratings[users_test], contact_index)
+
+print(f'{movie_ratings_train.shape[0]} samples for training')
+print(f'{movie_ratings_test.shape[0]} samples for testing')
+
+# %%
+
+X_train, Y_train = create_samples_from_ratings(movie_ratings_train, contact_index)
+X_test, Y_test = create_samples_from_ratings(movie_ratings_test, contact_index)
