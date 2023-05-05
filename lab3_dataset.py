96b5ad025f9727e296dd130eb5a1df2d931944b2
Pedro Batista <pedro.batista@ericsson.com>
2023-05-05 11:45:15 +0200

    Fixes dataset movie id mapping and adds validation

--- lab3_dataset.py
+++ lab3_dataset.py
@@ -2,6 +2,7 @@ import zipfile
 
 import numpy as np
 import torch
+from sklearn.model_selection import train_test_split
 
 from lab3_similarity_graph import (compute_movie_similarity_graph, stratify_and_normalize)
 from utils import random
@@ -23,7 +24,7 @@ def load_movie_dataset():
     movie_ratings[u_data[:, 0] - 1, u_data[:, 1] - 1] = u_data[:, 2]
 
     selected_movies_mask = np.sum(movie_ratings > 0, axis=0) >= 150
-    new_movie_ids = np.cumsum(selected_movies_mask)
+    new_movie_ids = np.cumsum(selected_movies_mask) - 1
     new_movie_id_map = dict(
         zip(np.arange(n_movies)[selected_movies_mask], new_movie_ids[selected_movies_mask]))
     cleaned_movie_ratings = movie_ratings[:, selected_movies_mask]
@@ -31,16 +32,19 @@ def load_movie_dataset():
     return cleaned_movie_ratings, new_movie_id_map
 
 
-def split_train_test_users(n_users, ratio_train=0.9, n_sets=1):
+def split_users(n_users, ratio_train=0.9, ratio_validation=0.09, n_sets=1):
     n_test = int(np.floor((1 - ratio_train) * n_users))
+    n_validation = int(np.floor(ratio_validation * n_users))
     random_indexes = np.arange(n_users)
     random.shuffle(random_indexes)
     random_indexes = random_indexes[:n_test * n_sets].reshape(-1, n_test)
     indexes = np.arange(n_users)
     for i in range(n_sets):
         users_test = random_indexes[i]
-        users_train = np.setdiff1d(indexes, users_test)
-        yield users_train, users_test
+        users_train, users_validation = train_test_split(np.setdiff1d(indexes, users_test),
+                                                         test_size=n_validation)
+        print(n_users, n_users - n_validation - n_test, n_validation, n_test)
+        yield users_train, users_validation, users_test
 
 
 def remove_users_not_rating_movie(movie_ratings, movie):
@@ -56,7 +60,7 @@ def create_samples_from_ratings(movie_ratings, target_movie):
     return X, Y
 
 
-def generate_dataset(users_train, users_test, target_movie):
+def generate_dataset(users_train, users_validation, users_test, target_movie):
     adjacency_matrix = compute_movie_similarity_graph(movie_ratings[users_train])
     if verbose:
         print(f'There are {adjacency_matrix.shape[0]} movies in the similarity graph')
@@ -64,6 +68,8 @@ def generate_dataset(users_train, users_test, target_movie):
     adjacency_matrix = stratify_and_normalize(adjacency_matrix)
 
     movie_ratings_train = remove_users_not_rating_movie(movie_ratings[users_train], target_movie)
+    movie_ratings_validation = remove_users_not_rating_movie(movie_ratings[users_validation],
+                                                             target_movie)
     movie_ratings_test = remove_users_not_rating_movie(movie_ratings[users_test], target_movie)
 
     if verbose:
@@ -71,13 +77,15 @@ def generate_dataset(users_train, users_test, target_movie):
         print(f'{movie_ratings_test.shape[0]} samples for testing')
 
     X_train, Y_train = create_samples_from_ratings(movie_ratings_train, target_movie)
+    X_validation, Y_validation = create_samples_from_ratings(movie_ratings_validation,
+                                                             target_movie)
     X_test, Y_test = create_samples_from_ratings(movie_ratings_test, target_movie)
 
-    adjacency_matrix, X_train, X_test, Y_train, Y_test = [
+    adjacency_matrix, X_train, X_test, X_validation, Y_train, Y_test, Y_validation = [
         torch.tensor(v, dtype=torch.float32)
-        for v in [adjacency_matrix, X_train, X_test, Y_train, Y_test]
+        for v in [adjacency_matrix, X_train, X_test, X_validation, Y_train, Y_test, Y_validation]
     ]
-    return adjacency_matrix, X_train, X_test, Y_train, Y_test
+    return adjacency_matrix, X_train, X_test, X_validation, Y_train, Y_test, Y_validation
 
 
 if __name__ == '__main__':
@@ -88,10 +96,12 @@ if __name__ == '__main__':
     contact_index = movie_id_map_loading[contact_index]
     n_users = movie_ratings.shape[0]
 
-    for i, (users_train,
-            users_test) in enumerate(split_train_test_users(n_users, ratio_train=.9, n_sets=5)):
-        adjacency_matrix, X_train, X_test, Y_train, Y_test = generate_dataset(
-            users_train, users_test, contact_index)
+    for i, (users_train, users_validation, users_test) in enumerate(
+            split_users(n_users, ratio_train=.9, ratio_validation=0.09, n_sets=8)):
+        adjacency_matrix, X_train, X_test, X_validation, Y_train, Y_test, Y_validation = generate_dataset(
+            users_train, users_validation, users_test, contact_index)
 
-        torch.save([contact_index, adjacency_matrix, X_train, X_test, Y_train, Y_test],
-                   f'datasets/lab3_similarity_{i:02d}.pt')
+        torch.save([
+            contact_index, adjacency_matrix, X_train, X_test, X_validation, Y_train, Y_test,
+            Y_validation
+        ], f'datasets/lab3_similarity_{i:02d}.pt')
