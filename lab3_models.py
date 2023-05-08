61ac3cbfcb9e88aeb4155eaab95041221ea07b9e
Pedro Batista <pedro.batista@ericsson.com>
2023-05-08 10:27:26 +0200

    Adds gnns and validation to lab3

--- lab3_models.py
+++ lab3_models.py
@@ -10,17 +10,19 @@ torch.random.manual_seed(20)
 
 
 # %%
-def make_estimate_single_movie_loss(movie_index):
+def make_estimate_single_movie_mse(movie_index):
     mse = torch.nn.MSELoss()
-    return lambda *y: mse(*y) / 2
+    return lambda y, pred: mse(y[..., movie_index], pred[..., movie_index])
 
 
 def run_experiment(target_movie,
                    adjacency_matrix,
                    X_train,
                    X_test,
+                   X_validation,
                    Y_train,
                    Y_test,
+                   Y_validation,
                    task,
                    save_prefix=None,
                    verbose=False,
@@ -28,8 +30,8 @@ def run_experiment(target_movie,
     n_nodes = X_train.shape[1]
     squeeze_feature_dims = False
     learning_rate = 5e-2
-    n_epochs = 80
-    batch_size = 200
+    n_epochs = 40
+    batch_size = 5
 
     if task == 'graph_filter':
         banks_order = [5, 1]
@@ -38,17 +40,44 @@ def run_experiment(target_movie,
                               n_filters_per_bank,
                               banks_order,
                               adjacency_matrix,
-                              use_activation=False)
+                              use_activation=False,
+                              use_bias=True)
     elif task == 'linear':
         squeeze_feature_dims = True
         model = torch.nn.Linear(n_nodes, n_nodes)
+    elif task == 'fcnn':
+        squeeze_feature_dims = True
+        model = torch.nn.Sequential(torch.nn.Linear(n_nodes, 64), torch.nn.LeakyReLU(),
+                                    torch.nn.Linear(64, n_nodes))
+    elif task == 'gnn':
+        banks_order = [5]
+        n_filters_per_bank = [64]
+        n_features_out = 1
+        model = MultiLayerGNN(1,
+                              n_filters_per_bank,
+                              banks_order,
+                              n_features_out,
+                              adjacency_matrix,
+                              use_activation=True,
+                              use_bias=True)
+    elif task == '2l_gnn':
+        banks_order = [5, 5]
+        n_filters_per_bank = [64, 32]
+        n_features_out = 1
+        model = MultiLayerGNN(1,
+                              n_filters_per_bank,
+                              banks_order,
+                              n_features_out,
+                              adjacency_matrix,
+                              use_activation=True,
+                              use_bias=True)
 
     if squeeze_feature_dims:
         X_train = torch.squeeze(X_train)
+        X_validation = torch.squeeze(X_validation)
         X_test = torch.squeeze(X_test)
 
     optimizer = torch.optim.Adam(model.parameters(), learning_rate)
-    loss_function = make_estimate_single_movie_loss(target_movie)
 
     train_model(model,
                 optimizer,
@@ -56,17 +85,22 @@ def run_experiment(target_movie,
                 batch_size,
                 X_train,
                 Y_train,
-                loss_function,
+                lambda *y: make_estimate_single_movie_mse(target_movie)(*y) / 2,
+                X_validation,
+                Y_validation,
                 save_prefix=save_prefix,
                 verbose=verbose)
 
-    test_loss, train_loss, _ = evaluate_model_loss(model,
-                                                   torch.nn.MSELoss(),
-                                                   X_test,
-                                                   Y_test,
-                                                   X_train,
-                                                   Y_train,
-                                                   verbose=verbose)
+    test_loss, train_loss, _ = evaluate_model_loss(
+        model,
+        lambda *y: torch.sqrt(make_estimate_single_movie_mse(target_movie)(*y)),
+        X_test,
+        Y_test,
+        X_train,
+        Y_train,
+        X_validation,
+        Y_validation,
+        verbose=verbose)
 
     if show_summary:
         summary(model)
