003a36fbcc98529ff35c0d48f2bbf915975673e9
Pedro Batista <pedro.batista@ericsson.com>
2023-05-08 13:37:37 +0200

    Runs test on graphs of different size for lab 3

--- lab3_main.py
+++ lab3_main.py
@@ -1,10 +1,13 @@
 import argparse
+import os
+import re
 from glob import glob
 
 import numpy as np
 import torch
 
 import lab3_models
+from training import evaluate_model_loss
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--experiments', nargs='+', help='Which experiments to run')
@@ -37,22 +40,43 @@ for name, experiment_args in experiments.items():
     if args.experiments is not None and name not in args.experiments:
         continue
 
-    results = []
+    results = {}
+    results['150_reviews'] = []
+    results['50_reviews'] = []
+    results['10_reviews'] = []
+    transferability_results = ['50_reviews', '10_reviews']
     for _ in range(n_experiments):
-        for dataset_name in glob(args.dataset_glob_pattern):
+        for i_dataset, dataset_name in enumerate(glob(args.dataset_glob_pattern)):
             dataset_args = torch.load(dataset_name)
 
             model_name = experiment_args['task']
-            experiment_args['save_prefix'] = f'{model_name}_{len(results):02d}'
-            experiment_args['show_summary'] = args.show_model_summary and len(results) == 0
-            results.append(lab3_models.run_experiment(
-                *dataset_args,
-                **experiment_args,
-            ))
-    results = np.array(results)
+            experiment_args['save_prefix'] = f'{model_name}_{i_dataset:02d}'
+            experiment_args['show_summary'] = args.show_model_summary and i_dataset == 0
+            results['150_reviews'].append(
+                lab3_models.run_experiment(
+                    *dataset_args,
+                    **experiment_args,
+                ))
+
+            for n_reviews_str in transferability_results:
+                model = torch.load(os.path.join('models', experiment_args['save_prefix'] + '.pt'))
+                target_movie, adjacency_matrix, _, X_test, _, _, Y_test, _ = torch.load(
+                    re.sub(r'150_reviews(\.pt$)', rf'{n_reviews_str}\1', dataset_name))
+                model.set_graph_shift_operator(adjacency_matrix)
+                test_loss = evaluate_model_loss(model, lab3_models.make_eval_loss(target_movie),
+                                                X_test, Y_test)[0]
+                results[n_reviews_str].append(test_loss)
+
+    result_array = np.array(results['150_reviews'])
     print('Train')
-    print(f'{np.mean(results[:, 1]):.5f} - {name} -',
-          ''.join([f'{v:.3f} ' for v in results[:, 1]]))
+
+    def show_result(name, result):
+        print(f'{np.mean(result):.5f} - {name} -', ''.join([f'{v:.3f} ' for v in result]))
+
+    show_result(name, result_array[:, 1])
     print('Test')
-    print(f'{np.mean(results[:, 0]):.5f} - {name} -',
-          ''.join([f'{v:.3f} ' for v in results[:, 0]]))
+    show_result(name, result_array[:, 0])
+
+    for n_reviews_str in transferability_results:
+        result_array = np.array(results[n_reviews_str])
+        show_result(n_reviews_str, result_array)
